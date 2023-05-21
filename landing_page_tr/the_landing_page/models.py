327728c05600a6cd7c61a3d88fcbe40e9478884a
David Mukajanga <mukajangadavid@gmail.com>
2023-05-21 10:37:01 +0300

    Established a page with Django and installed "pillow".

--- landing_page_tr/the_landing_page/models.py
+++ landing_page_tr/the_landing_page/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Kurslar(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="imgs/")
